1be8c4f134c4b132b06a2a896c16f83be3ed3adc
David Liptai <dliptai@swin.edu.au>
2023-06-19 16:26:25 +1000

    separate the install and update steps + increase timeout

--- jupyterhub/Resources/scripts/install.sh
+++ jupyterhub/Resources/scripts/install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -xeu
-
 # Script to install and set up The Littlest JupyterHub
 
-while ! apt-get remove -y unattended-upgrades; do sleep 1; done
-apt-get purge -y unattended-upgrades
-systemctl stop unattended-upgrades || true
-systemctl disable unattended-upgrades || true
-apt-get update -y
 apt-get install -y ansible
 
 url="https://api.github.com/repos/ADACS-Australia/openstack-tljh/releases/latest"
@@ -15,4 +9,4 @@ fname="openstack-tljh.tar.gz"
 wget "$location" -O "$fname"
 tar -xvzf "$fname"
 
-ansible-playbook -c local -i localhost, ADACS-Australia-openstack-tljh*/playbook.yml
+ansible-playbook -vv -c local -i localhost, ADACS-Australia-openstack-tljh*/playbook.yml
